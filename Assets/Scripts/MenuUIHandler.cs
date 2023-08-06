0f23bdb101ef2e69dd0d41c121249af0176d7985
AsadSaied <141374266+AsadSaied@users.noreply.github.com>
2023-08-07 00:58:42 +0300

    title screen is done
    - added a title screen
    - added a text field for the player name
    - added play button with load screen code commented

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Loading Scene");
+        //SceneManager.LoadScene(1);
+    }
+}
